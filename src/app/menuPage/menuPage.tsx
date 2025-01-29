81726c861a4d1a6cc5f40c6dd3c928854e59aab6
Franco Armando <francoarmando1911@gmail.com>
2025-01-29 17:08:01 -0300

    Inicializacion de proyecto y repositorio

---
+++ src/app/menuPage/menuPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function menuPage() {
+  return (
+    <div className='bg-violet-500'>
+        <h1 className='font-bold text-6xl text-red-900'>This is the menu page</h1>
+    </div>
+  )
+}
